5147179c27a1dab622b9e667a734302d8a8a4827
Myzwer <josh@onefortyfivedesign.com>
2021-06-29 16:53:18 -0400

    :zap: Configured TailwindCSS to purge unused styles. Had to change the body and add a viewport (in the header) to be able to properly check it.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         'postcss-preset-env',
-        'tailwindcss',
-        'autoprefixer'
+        require('tailwindcss'),
+        require('autoprefixer'),
     ],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    // This is where we set Tailwind's Purge feature. Basically here we tell tailwind to look for any classes not listed
+    // in the files below, and purge everything else out.
+    // In this case, we want any top level php files (all of our wp theme files) as well as any source js.
+    purge: [
+        '*.php',
+        './assets/src/js/*.js',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
