57c601fd02153a315c4cb1c2cfd62b8750863fbe
Myzwer <josh@onefortyfivedesign.com>
2021-06-29 21:44:40 -0400

    :rocket: Enabled JIT mode in Tailwind, an experimental tech from Tailwind 2.1. I also updated the comments to further explain what was going on.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-    // This is where we set Tailwind's Purge feature. Basically here we tell tailwind to look for any classes not listed
-    // in the files below, and purge everything else out.
-    // In this case, we want any top level php files (all of our wp theme files) as well as any source js.
+    /*
+    * Enable JIT mode, a bleeding edge experimental feature released with tailwind v2.1
+    * If you don't want to use this, just delete the line of code.
+    *
+    * https://tailwindcss.com/docs/just-in-time-mode#enabling-jit-mode
+    */
+    mode: 'jit',
+
+
+    /*
+    * So the purge feature. Super cool.
+    * Below you need to set the purge location. This basically means where Tailwind needs to look for classes.
+    * It might seem backwards, but its checking those files for things NOT to purge.
+    * Unless you need to change it, you should be able to just leave this alone. It checks the php and js files.
+    *
+    * JIT MODE UPDATE: Does the same thing, but its instead looking for classes to ADD, not purge.
+    */
     purge: [
         '*.php',
         './assets/src/js/*.js',
@@ -19,6 +33,7 @@ module.exports = {
              * Light / Dark are variants within the same palette
              * Classes are named text-CLASSNAME
              *
+             * https://tailwindcss.com/docs/customizing-colors#custom-colors
              * */
             blue: {
                 light: '#6495ED', //text-blue-light
