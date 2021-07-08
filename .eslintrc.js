b3b49fd644b75ca08619b96996800d6a19e99f7b
Myzwer <josh@onefortyfivedesign.com>
2021-07-08 11:45:12 -0400

    :zap: Added more rules to Stylelint to customize it to my standards.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,13 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: [ 'google', 'plugin:@wordpress/eslint-plugin/recommended' ],
+	extends: ['google', 'plugin:@wordpress/eslint-plugin/recommended'],
 	parserOptions: {
 		ecmaVersion: 12,
 		sourceType: 'module',
 	},
 	rules: {
-		'max-len': [ 2, 120, 4, { ignoreUrls: true } ],
+		'max-len': [2, 120, 4, { ignoreUrls: true }],
 	},
 	globals: {
 		wp: true,
