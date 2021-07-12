446a5945f20be919b7b846e70ac13f0ba4fa7406
Myzwer <josh@onefortyfivedesign.com>
2021-07-12 16:00:58 -0400

    :rocket: Added CSSnano to handle the css minification for production. Also updated a lot of the comments on the webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+// ****** THE WEBPACK CONFIG FILE ******
+// *
+// * I have commented this file heavily, almost to the point of making the code hard to read.
+// * This is because I hated seeing guides online with barely any context as to what any of this did
+// * You may not understand how to change this stuff, but hopefully you can at least see what its doing.
+
 // ***** WEBPACK'S DEMANDS *****
-// * Not really a list of demands but also kind of that. This is where we tell Webpack what it needs to require.
+// * Lol ok not really a list of demands but also kind of that.
+// * This is where we tell Webpack what it needs to require.
 const path = require("path");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const StylelintPlugin = require("stylelint-webpack-plugin");
+const CssnanoPlugin = require("cssnano-webpack-plugin");
 
 // ***** SET ENVIRONMENT *****
 // * This tells webpack to always run in development mode.
@@ -21,6 +29,7 @@ module.exports = {
 
   module: {
     rules: [
+      // *** CSS ***
       {
         test: /\.s?css$/i,
         use: [
@@ -31,6 +40,8 @@ module.exports = {
           "sass-loader",
         ],
       },
+
+      // *** BABEL ***
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -38,6 +49,8 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+
+      // *** FONTS ***
       {
         test: /\.(ttf|eot|woff|woff2|svg)$/,
         use: {
@@ -52,7 +65,30 @@ module.exports = {
     ],
   },
 
+  // * CSS Minimization!
+  // * So this is where CSSNano is configured to minimize our CSS.
+  // * It does a few extra goodies like removing comments and stuff
+  // * Currently using defaul settings. More on that here:
+  // * https://cssnano.co/docs/optimisations
+  optimization: {
+    minimizer: [
+      new CssnanoPlugin({
+        test: /\.s?css$/i,
+        cssnanoOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ],
+  },
+
+  // *** INPUT / OUTPUT ***
   // * This is how to change entry location as well as output location and name
+  // * Relative to this file, tell it where your js is coming from and where its going.
   entry: "./assets/src/js/frontend.js",
   output: {
     filename: "frontend.js",
@@ -66,33 +102,34 @@ module.exports = {
 
   // ***** SET PLUGINS *****
   plugins: [
+    // *** BROWSERSYNC ***
     new BrowserSyncPlugin(
-      // BrowserSync options
       {
-        // browse to http://localhost:3000/ during development
         host: "localhost",
         port: 3000,
-        // proxy the Webpack Dev Server endpoint
-        // (which should be serving on http://localhost:3100/)
-        // through BrowserSync
 
-        // YOU NEED TO CHANGE THIS. Local is going to output a Site Domain that ends in .local
-        // That needs to be pasted here to get Browersync to work properly.
+        // * YOU NEED TO CHANGE THIS. VVV
+        // * Local is going to output a Site Domain that ends in .local
+        // * That needs to be pasted here to get Browersync to work properly.
         proxy: "prelaunch.local",
+        // * CHANGE ME ^^^
       },
-      // plugin options
+
       {
-        // prevent BrowserSync from reloading the page
-        // and let Webpack Dev Server take care of this
+        // * Prevent BrowserSync from reloading the page
+        // * This lets Webpack Dev Server take care of this
+        // * Unless you have a reason to change it, leave this alone.
         reload: false,
       }
     ),
 
+    // *** STYLELINT ***
     new StylelintPlugin(),
 
+    // *** MINI CSS EXTRACT PLUGIN ***
     // * Lets get that CSS rolling. This plugin lets Webpack actually build a CSS file that's separate.
     // * If you want that file to be somewhere else, the path below will change that.
-    // * The path is relevant to output.path (the folder where frontend.js is).
+    // * The path is relative to output.path (the folder where frontend.js is).
     new MiniCssExtractPlugin({
       filename: "../css/frontend.css",
     }),
