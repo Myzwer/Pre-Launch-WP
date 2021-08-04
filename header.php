d00be1e18721a8ad6085985302b92c47e37e6489
Myzwer <josh@onefortyfivedesign.com>
2021-08-04 10:25:06 -0400

    :rocket: Navbar is working in wordpress with the navwalker, still need to refactor the code to remove old unused code as well as add the dropdown. But the navbar is stable for now.

--- header.php
+++ header.php
@@ -32,64 +32,26 @@
                 src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png"
                 alt=""></div>
     <label for="drop" class="toggle">
-        <div class = "borger-menu">
+        <div class="borger-menu">
             <span></span>
             <span></span>
             <span></span>
             <span></span>
         </div>
     </label>
-    <input type="checkbox" id="drop"/>
-    <ul class="menu">
-        <li><a href="#">Home</a></li>
-        <li>
-            <!-- First Tier Drop Down -->
-            <label for="drop-1" class="toggle">Service</label>
-            <a href="#">Service</a>
-            <input type="checkbox" id="drop-1"/>
-            <ul>
-                <li><a href="#">Service 1</a></li>
-                <li><a href="#">Service 2</a></li>
-                <li><a href="#">Service 3</a></li>
-            </ul>
-        </li>
-        <li>
 
-            <!-- First Tier Drop Down -->
-            <label for="drop-2" class="toggle">Portfolio</label>
-            <a href="#">Portfolio</a>
-            <input type="checkbox" id="drop-2"/>
-            <ul>
-                <li><a href="#">Portfolio 1</a></li>
-                <li><a href="#">Portfolio 2</a></li>
-                <li>
+    <?php
+    wp_nav_menu(array(
+    'theme_location' => 'header-menu',
+    'menu_class' => 'primary-menu', // pass whatever classes to be added to top level here
+    'walker' => new PreLaunch_Walker(),
+    'items_wrap' => '<input type="checkbox" id="drop"/><ul class = \'menu %2$s\'>%3$s</ul>'
 
-                    <!-- Second Tier Drop Down -->
-                    <label for="drop-3" class="toggle">Works</label>
-                    <a href="#">Works</a>
-                    <input type="checkbox" id="drop-3"/>
-                    <ul>
-                        <li><a href="#">HTML/CSS</a></li>
-                        <li><a href="#">jQuery</a></li>
-                        <li><a href="#">Python</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Submit</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">About</a></li>
-    </ul>
+    ));
+    ?>
+
 </nav>
 
-<?php
-/* wp_nav_menu(array(
-     'theme_location' => 'header-menu',
-     'menu_class'     => 'primary-menu', // pass whatever classes to be added to top level here
-     'walker' => new PreLaunch_Walker(),
-     'items_wrap' => '<nav role="navigation" class="%2$s">%3$s</nav>'
- ));*/
-?>
+
 
 
--- nav_walker.php
+++ nav_walker.php
@@ -21,11 +21,12 @@ class PreLaunch_Walker extends Walker_Nav_Menu
         $title = $item->title;
         $permalink = $item->url;
         if ($depth === 0) {
-            $classes = "px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline";
-        } else {
-            $classes = "block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline";
+            $classes = "cat";
         }
-        $output .= "<a class='$classes' href='$permalink'>$title</a>";
+        else {
+            $classes = "cat-2";
+        }
+        $output .= "<li><a href='$permalink'>$title</a></li>";
 
 
        /* $object = $item->object;
@@ -88,13 +89,7 @@ class PreLaunch_Walker extends Walker_Nav_Menu
      */
     public function start_lvl(&$output, $depth = 0, $args = null)
     {
-        $output .= '<div @click.away="open = false" class="relative" x-data="{ open: false }">';
-        $output .= '<button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">';
-        $output .= '<span>Dropdown</span>';
-        $output .= '<svg fill="currentColor" viewBox="0 0 20 20" :class="{\'rotate-180\': open, \'rotate-0\': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>';
-        $output .= '</button>';
-        $output .= '<div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">';
-        $output .= '<div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">';
+        $output .= '<div>';
     }
 
     /**
