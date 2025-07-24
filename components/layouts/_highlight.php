5123e72ade6bc9ec1ad789c06893dc4d7a59b59d
Myzwer <josh@onefortyfivedesign.com>
2025-07-24 00:36:08 -0400

    :white_check_mark: Add highlight layout component
    Created a new reusable layout component to display highlights. This component uses ACF fields for dynamic content and ensures responsive design with Tailwind classes.

---
+++ components/layouts/_highlight.php
@@ -0,0 +1,31 @@
+<!-- Start Highlights -->
+<div class="bg-black text-white">
+    <div class="grid grid-cols-12 gap-4 py-5 md:py-0">
+        <div class="col-span-12 md:col-span-4 md:border-r">
+            <div class="text-center md:py-5 font-bold uppercase">
+                <?php the_field('highlight_1') ?>
+            </div>
+        </div>
+
+        <div class="col-span-12 md:hidden">
+            <hr>
+        </div>
+
+        <div class="col-span-12 md:col-span-4 md:border-r">
+            <div class="text-center md:py-5 font-bold uppercase">
+                <?php the_field('highlight_2') ?>
+            </div>
+        </div>
+
+        <div class="col-span-12 md:hidden">
+            <hr>
+        </div>
+
+        <div class="col-span-12 md:col-span-4">
+            <div class="text-center md:py-5 font-bold uppercase">
+                <?php the_field('highlight_3') ?>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Highlights -->
