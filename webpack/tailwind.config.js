668fdf0e1d2c3ef83c902331c438189b7d00e024
Myzwer <josh@onefortyfivedesign.com>
2025-07-23 23:53:21 -0400

    :repeat: Refactor Tailwind config for improved readability
    Reorganized screen definitions and comments in the Tailwind config. Simplified the structure by ensuring screens and colors were in the correct hierarchy.

--- webpack/tailwind.config.js
+++ webpack/tailwind.config.js
@@ -10,32 +10,32 @@ module.exports = {
    */
   content: ["*.php", "./components/**/*.php", "./assets/src/js/*.js"],
   theme: {
+    screens: {
+      sm: "39.9375em",
+      // => @media (min-width: 640px) { ... }
+
+      md: "63.9375em",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "64em",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "74.9375em",
+      // => @media (min-width: 1280px) { ... }
+    },
+    transparent: "transparent",
+    current: "currentColor",
+    /* How to Add custom Colors
+     * Give your color a name, make it something that makes sense.
+     * If you aren't going to use the colors below, delete them.
+     *
+     * Default is the normal base color.
+     * Light / Dark are variants within the same palette
+     * Classes are named text-CLASSNAME
+     *
+     * https://tailwindcss.com/docs/customizing-colors#custom-colors
+     * */
     colors: {
-      screens: {
-        sm: "39.9375em",
-        // => @media (min-width: 640px) { ... }
-
-        md: "63.9375em",
-        // => @media (min-width: 768px) { ... }
-
-        lg: "64em",
-        // => @media (min-width: 1024px) { ... }
-
-        xl: "74.9375em",
-        // => @media (min-width: 1280px) { ... }
-      },
-      transparent: "transparent",
-      current: "currentColor",
-      /* How to Add custom Colors
-       * Give your color a name, make it something that makes sense.
-       * If you aren't going to use the colors below, delete them.
-       *
-       * Default is the normal base color.
-       * Light / Dark are variants within the same palette
-       * Classes are named text-CLASSNAME
-       *
-       * https://tailwindcss.com/docs/customizing-colors#custom-colors
-       * */
       blue: {
         light: "#6495ED", // text-blue-light
         DEFAULT: "#0047AB", // text-blue
