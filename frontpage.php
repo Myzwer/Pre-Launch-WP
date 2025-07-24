b49cce477d7195f33b3d116e4d487a98db3c4151
Myzwer <josh@onefortyfivedesign.com>
2025-07-24 00:31:42 -0400

    :repeat: Replace static hero section with dynamic template part
    Replaced a hardcoded hero section in `frontpage.php` with a dynamic template part component. This improves maintainability, reusability, and adheres to WordPress best practices.

--- frontpage.php
+++ frontpage.php
@@ -14,18 +14,8 @@
 
 get_header(); ?>
 
-    <video class="header-video" src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop playsinline muted></video>
+<?php get_template_part('components/headers/_hero'); ?>
 
-    <div class="viewport-header">
-        <div class="head-container">
-            <div class="center add-padding">
-                <h1 class="text-white text-5xl pb-5">Header Title</h1>
-            </div>
-            <hr class="text-white pb-5">
-            <h2 class="text-white text-3xl ">Title</h2>
-            <h3 class="text-white text-2xl">Subtitle</h3>
-        </div>
-    </div>
 
     <div class="bg-white md:p-10">
         <div class=" lg:max-w-5xl lg:mx-auto">
