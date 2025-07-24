459da89ddcead38a86974620445d9e8d63939416
Myzwer <josh@onefortyfivedesign.com>
2025-07-24 00:29:26 -0400

    :white_check_mark: Add hero component with video background and content
    Introduced a new hero component featuring a video background, title, subtitle, and a call-to-action button. This enhances the header section to support dynamic and visually engaging content.

---
+++ components/headers/_hero.php
@@ -0,0 +1,11 @@
+<video class="header-video" src="<?php the_field('video_background'); ?>" autoplay loop playsinline muted></video>
+
+<div class="viewport-header">
+    <div class="text-center p-5">
+        <h1 class="text-white text-5xl pb-5"><?php the_field('title'); ?></h1>
+        <h3 class="text-white text-2xl pb-10"><?php the_field('subtitle'); ?></h3>
+        <a href="/contact" class="fab-main">
+            <i class="fa-solid fa-circle-arrow-right"></i> Button
+        </a>
+    </div>
+</div>
