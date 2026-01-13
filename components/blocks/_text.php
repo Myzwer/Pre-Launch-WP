becdc66193311e73dfc45350aac0ca2c49892a7c
Myzwer <josh@onefortyfivedesign.com>
2026-01-13 00:48:01 -0500

    :repeat: Standardize PHP formatting for improved readability
    Adjusted PHP code indentation and spacing for consistency. This enhances clarity and makes the code easier to maintain.

--- components/blocks/_text.php
+++ components/blocks/_text.php
@@ -17,16 +17,16 @@
 ?>
 
 <div class="p-5 mx-auto max-w-screen-2xl xl:p-5 xl:w-8/12">
-    <div class="grid grid-cols-12 gap-4 md:gap-4">
-        <div class="col-span-12 py-5 max-w-none prose">
-            <?php
-            // Check if the code needs to use field (top level) or subfield (in flex content)
-            if(get_sub_field("text_editor")): the_sub_field("text_editor");
-            elseif(get_field("text_editor")): the_field("text_editor");
-            endif;
-            ?>
-        </div>
-    </div>
+	<div class="grid grid-cols-12 gap-4 md:gap-4">
+		<div class="col-span-12 py-5 max-w-none prose">
+			<?php
+			// Check if the code needs to use field (top level) or subfield (in flex content)
+			if( get_sub_field( "text_editor" ) ): the_sub_field( "text_editor" );
+			elseif( get_field( "text_editor" ) ): the_field( "text_editor" );
+			endif;
+			?>
+		</div>
+	</div>
 </div>
 
 
