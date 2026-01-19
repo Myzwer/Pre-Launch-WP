e267414c37e28cef5ca19017f17bcb1168ab20a8
Myzwer <josh@onefortyfivedesign.com>
2026-01-19 00:53:45 -0500

    :rocket: Add primary navigation initialization on DOMContentLoaded
    Integrated the `initPrimaryNav` function to initialize the primary navigation when the DOM is fully loaded. Ensures smoother navigation setup and aligns with page load events.

--- assets/src/js/frontend.ts
+++ assets/src/js/frontend.ts
@@ -2,4 +2,10 @@ import "../css/tailwind.css";
 import "../css/frontend.css";
 import "./accordion";
 
+import { initPrimaryNav } from "./nav";
+
+document.addEventListener("DOMContentLoaded", () => {
+	initPrimaryNav();
+});
+
 
