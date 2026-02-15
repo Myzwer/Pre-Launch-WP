ac3c0d0310885dbc39973638c947c7256f795609
Myzwer <josh@onefortyfivedesign.com>
2026-02-14 19:05:55 -0500

    :art: Improve search form styling and accessibility
    Refactored the search form for better readability, replaced outdated classes with standardized ones, and added ARIA attributes for improved accessibility. Updated label and button elements to align with modern semantic conventions.

--- searchform.php
+++ searchform.php
@@ -2,28 +2,32 @@
 /**
  * Search form template.
  *
- * WordPress will use this file automatically when calling get_search_form().
+ * Used in header, archives, and search result fallbacks.
  *
  * @link https://developer.wordpress.org/reference/functions/get_search_form/
  */
 ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
-	<label class="search-form__label">
-		<span class="screen-reader-text">
-			<?php esc_html_e('Search for:', 'prelaunch-wp'); ?>
-		</span>
+<form role="search"
+	  method="get"
+	  class="search-form"
+	  action="<?php echo esc_url(home_url('/')); ?>">
 
-		<input
-			type="search"
-			class="search-form__input"
-			placeholder="<?php echo esc_attr_x('Search…', 'placeholder', 'prelaunch-wp'); ?>"
-			value="<?php echo esc_attr(get_search_query()); ?>"
-			name="s"
-		/>
+	<label class="sr-only" for="search-field">
+		<?php esc_html_e('Search for:', 'prelaunch-wp'); ?>
 	</label>
 
-	<button type="submit" class="search-form__submit">
-		<?php echo esc_html_x('Search', 'submit button', 'prelaunch-wp'); ?>
+	<input
+		type="search"
+		id="search-field"
+		class="search-field"
+		placeholder="<?php echo esc_attr_x('Search…', 'placeholder', 'prelaunch-wp'); ?>"
+		value="<?php echo esc_attr(get_search_query()); ?>"
+		name="s"
+	/>
+
+	<button type="submit" class="search-submit">
+		<span class="sr-only"><?php esc_html_e('Submit search', 'prelaunch-wp'); ?></span>
+		<i class="fa-solid fa-magnifying-glass" aria-hidden="true"></i>
 	</button>
 </form>
