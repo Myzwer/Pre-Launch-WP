dffa58f4cd18e51158365fdf61b3350df4dec0ea
Myzwer <josh@onefortyfivedesign.com>
2026-02-21 18:14:49 -0500

    :construction: Make a few quick tests to confirm background.css works, will be updated for real later.

--- components/headers/_simple.php
+++ components/headers/_simple.php
@@ -32,22 +32,22 @@
     </div>
 </div>
 
-<!--
-Textured background?
-<div class="bg-white-gradient">
-    <div class="relative bg-scroll bg-no-repeat bg-cover"
-         style="background:
-                 url('<?php /*echo get_template_directory_uri(); */?>/assets/src/img/topography.png') center center;
-                 height: 20vh;">
-        <div class="text-center content-middle">
-            <h1 class="text-3xl font-bold text-white uppercase md:text-5xl">
-                <?php
-/*                // This enables simple_title to work regardless of its a top level field or a subfield in flex content
-                if(get_sub_field('simple_title')): the_sub_field('simple_title');
-                elseif(get_field('simple_title')): the_field('simple_title');
-                endif;
-                */?>
-            </h1>
-        </div>
-    </div>
-</div>-->
+
+<?php
+// Textured background
+$texture = get_field('textured_image', 'option');
+$texture_url = is_array($texture) ? ($texture['url'] ?? '') : $texture; // supports return: array or url
+?>
+
+<div class="bg-secondary-gradient">
+	<div
+		class="relative bg-texture"
+		style="<?php echo $texture_url ? '--bg-texture: url(\'' . esc_url($texture_url) . '\');' : ''; ?> height: 20vh;"
+	>
+		<div class="text-center content-middle">
+			<h1 class="text-3xl font-bold text-black uppercase md:text-5xl">
+				<?php echo $texture ?>
+			</h1>
+		</div>
+	</div>
+</div>
--- components/headers/flexy/_image.php
+++ components/headers/flexy/_image.php
@@ -11,47 +11,12 @@
  * */
 ?>
 
-<div class="relative bg-scroll bg-no-repeat bg-cover" style="background: linear-gradient(
-  rgba(0, 0, 0, 0.45),
-  rgba(0, 0, 0, 0.45)
-), url('<?php the_sub_field( "side_photo" ); ?>') center center; background-repeat: no-repeat; background-size: cover;
- height: 60vh;">
-    <div class="px-5 text-center content-middle-medium text-pretty">
-        <div class="center add-padding">
-            <h2 class="text-xl font-bold text-white lg:text-2xl lb-2"><?php the_sub_field( "small_subtitle" ); ?></h2>
-        </div>
-        <h1 class="text-3xl font-bold text-white uppercase lg:text-5xl"><?php the_sub_field( "main_title" ); ?></h1>
 
-
-        <?php if ( have_rows( 'primary_cta' ) ): ?>
-            <?php while ( have_rows( 'primary_cta' ) ): the_row(); ?>
-
-                <?php
-                $link      = get_sub_field( 'button_link' ) ?? get_sub_field( 'button_link_file' );
-
-                // Hide button if link is returning null
-                if ( $link ):
-                    // Get tab status
-                    $new_tab = get_sub_field( 'new_tab' );
-                    $attrs = '';
-                    if ( $new_tab == "yes" ) {
-                        $attrs = 'target="_blank"';
-                    } ?>
-
-                    <div class="mt-3">
-                        <a href="<?php echo esc_url( $link ); ?>" <?php echo $attrs; ?>
-                           class="btn-main">
-                            <i class="fa-solid fa-circle-arrow-right"></i> <?php the_sub_field( "button_text" ); ?>
-                        </a>
-                    </div>
-
-                <?php endif; // Close the if $link block ?>
-
-            <?php endwhile; // Close the while loop ?>
-        <?php endif; // Close the if have_rows block ?>
-
-    </div>
+<div class="relative bg-media bg-overlay-45" style="--bg-image: url('<?php echo esc_url(get_sub_field('side_photo')); ?>'); height: 60vh;">
+	<div class="px-5 text-center content-middle-medium text-pretty">
+		<h2 class="text-xl font-bold text-white lg:text-2xl lb-2"><?php the_sub_field( "small_subtitle" ); ?></h2>
+		<h1 class="text-3xl font-bold text-white uppercase lg:text-5xl"><?php the_sub_field( "main_title" ); ?></h1>
+	</div>
 </div>
-
 
 
