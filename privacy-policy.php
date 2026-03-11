f39f9f527bafa49d8454aafbc857be61b321d96a
Myzwer <josh@onefortyfivedesign.com>
2026-03-11 14:12:42 -0400

    :repeat: Refactor privacy-policy template with dynamic ACF fields
    Updated the privacy-policy template to use dynamic ACF fields for the header image and content. Simplified the structure, improved markup readability, and enhanced flexibility with dynamic data integration.

--- privacy-policy.php
+++ privacy-policy.php
@@ -1,41 +1,32 @@
 <?php
-/**
- * Template Name: Privacy Policy
- *
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- *
- * @package WordPress
- * @subpackage Pre_Launch_WP
- * @since 1.0.0
- */
+	/**
+	 * Template Name: Policy Page
+	 *
+	 *
+	 * @docs https://codex.wordpress.org/Template_Hierarchy
+	 */
 
-get_header(); ?>
+	get_header(); ?>
 
-    <div class="relative bg-scroll bg-no-repeat bg-cover" style="background: linear-gradient(
-  rgba(0, 0, 0, 0.45),
-  rgba(0, 0, 0, 0.45)
-), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
- height: 60vh;">
-        <div class="text-center text-white content-middle">
-            <h1 class="mb-5 text-4xl">Privacy Policy</h1>
-        </div>
-    </div>
+	<div class="relative bg-scroll bg-no-repeat bg-cover" style="background: linear-gradient(
+		rgba(0, 0, 0, 0.45),
+		rgba(0, 0, 0, 0.45)
+		), url('<?php the_field( 'header_image' ); ?>') center center;
+		height: 40vh;">
+		<div class="text-center text-white content-middle">
+			<h1 class="mb-5 text-4xl font-bold"><?php echo the_title(); ?></h1>
+		</div>
+	</div>
 
 
-    <div class="m-4 md:m-10 lg:mx-auto lg:max-w-4xl privacy-policy">
-        <div class="grid grid-cols-12 gap-4">
-	        <div class="col-span-12 p-5">
-			<?php if (have_posts()) : while (have_posts()) : the_post();
-			    the_content();
-			endwhile;
-			else: ?>
-                <p>Sorry, no posts matched your criteria.</p>
-			<?php endif; ?>
-	        </div>
-        </div>
-    </div>
+	<div class="wrap">
+		<div class="grid-12">
+			<div class="col-span-12 py-10 prose-theme">
+				<?php the_field( 'policy' ); ?>
+				Contact Email: <?php echo esc_html( get_option( 'admin_email' ) ); ?>
+			</div>
+		</div>
+	</div>
 
 
 <?php get_footer();
