e8f28cb4f37932bbac23e22a102144101d542254
Myzwer <josh@onefortyfivedesign.com>
2026-04-05 11:28:05 -0400

    :fire: Clean up files and locations
    Deleted old flex content sections that are no longer needed and moved the rest to a better folder structure.

--- page.php
+++ page.php
@@ -1,80 +1,80 @@
 <?php
-/**
- * The Default template file - Page Builder
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Pre_Launch_WP
- * @author Josh Forrester <josh@onefortyfivedesign.com>
- * @version 1.0.0
- */
+	/**
+	 * The Default template file - Page Builder
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+	 *
+	 * @package WordPress
+	 * @subpackage Pre_Launch_WP
+	 * @author Josh Forrester <josh@onefortyfivedesign.com>
+	 * @version 1.0.0
+	 */
 
-get_header(); ?>
+	get_header(); ?>
 
 <?php
 // Check value exists.
-if (have_rows('header_select')) :
+	if ( have_rows( 'header_select' ) ) :
 
-    // Loop through rows.
-    while (have_rows('header_select')) : the_row();
+		// Loop through rows.
+		while ( have_rows( 'header_select' ) ) : the_row();
 
-        switch (get_row_layout()) {
-            case 'simple_header':
-                get_template_part('components/headers/flexy/_simple');
-                break;
+			switch ( get_row_layout() ) {
+				case 'simple_header':
+//                get_template_part('components/headers/flexy/_simple');
+					break;
 
-            case 'button_header':
-                get_template_part('components/headers/flexy/_button');
-                break;
+				case 'button_header':
+//                get_template_part('components/headers/flexy/_button');
+					break;
 
-            case 'image_header':
-                get_template_part('components/headers/flexy/_image');
-                break;
+				case 'image_header':
+//                get_template_part('components/headers/flexy/_image');
+					break;
 
-            default:
-                error_log('Unhandled content block: ' . get_row_layout());
-                break;
-        }
+				default:
+					error_log( 'Unhandled content block: ' . get_row_layout() );
+					break;
+			}
 
-    endwhile;
-endif;
+		endwhile;
+	endif;
 ?>
 
 
 <?php
 // Check value exists.
-if (have_rows('body_sections')) :
+	if ( have_rows( 'body_sections' ) ) :
 
-    echo "<div class='alt-bg-wrap'>"; // Wrap the entire section
+		echo "<div class='alt-bg-wrap'>"; // Wrap the entire section
 
-    // Loop through rows.
-    while (have_rows('body_sections')) : the_row();
+		// Loop through rows.
+		while ( have_rows( 'body_sections' ) ) : the_row();
 
-        echo "<div class='bg-alternating-gradient'>";
+			echo "<div class='bg-alternating-gradient'>";
 
-        switch (get_row_layout()) {
-            case 'text_block':
-                get_template_part('components/blocks/flexy/_text');
-                break;
+			switch ( get_row_layout() ) {
+				case 'text_block':
+//                get_template_part('components/blocks/flexy/_text');
+					break;
 
-            case 'image_banner_text':
-                get_template_part('components/blocks/flexy/_image-text');
-                break;
+				case 'image_banner_text':
+//                get_template_part('components/blocks/flexy/_image-text');
+					break;
 
-            default:
-                error_log('Unhandled content block: ' . get_row_layout());
-                break;
-        }
+				default:
+					error_log( 'Unhandled content block: ' . get_row_layout() );
+					break;
+			}
 
-        echo '</div>';
+			echo '</div>';
 
-        // End loop.
-    endwhile;
+			// End loop.
+		endwhile;
 
-    echo '</div>';
+		echo '</div>';
 
-endif;
+	endif;
 ?>
 
 <?php get_footer();
