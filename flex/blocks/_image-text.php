9614c82d01869ddb0419027a721261e941b2ac92
Myzwer <josh@onefortyfivedesign.com>
2026-04-26 12:13:01 -0400

    :repeat: Refactor image-text block for better structure and styling
    Replaced `$intro` with `$content` for semantic alignment and consistency. Updated HTML to a grid-based structure, added utility classes for styling, and improved image rendering with additional attributes.

--- flex/blocks/_image-text.php
+++ flex/blocks/_image-text.php
@@ -22,22 +22,28 @@
 	}
 
 
-	$header = get_sub_field( 'header' );
-	$intro  = get_sub_field( 'intro' );
-	$image  = get_sub_field( 'image' );
+	$content = get_sub_field( 'content' );
+	$image   = get_sub_field( 'image' );
 ?>
-<section class="flex-block flex-block--image-text">
-	<?php if ( $header ) : ?>
-		<h2><?php echo esc_html( $header ); ?></h2>
-	<?php endif; ?>
-
-	<?php if ( $intro ) : ?>
-		<div><?php echo wp_kses_post( $intro ); ?></div>
-	<?php endif; ?>
-
-	<?php if ( ! empty( $image['ID'] ) ) : ?>
-		<div>
-			<?php echo wp_get_attachment_image( (int) $image['ID'], 'large' ); ?>
+<section class="py-5 wrap">
+	<div class="grid-12 prose-theme">
+		<div class="col-span-12">
+			<?php if ( ! empty( $image['ID'] ) ) :
+				echo wp_get_attachment_image(
+					(int) $image['ID'],
+					'large',
+					false,
+					[
+						'class' => 'rounded-lg shadow-lg mb-0'
+					]
+				);
+			endif; ?>
 		</div>
-	<?php endif; ?>
+
+		<div class="col-span-12">
+			<?php if ( $content ) : ?>
+				<div><?php echo wp_kses_post( $content ); ?></div>
+			<?php endif; ?>
+		</div>
+	</div>
 </section>
