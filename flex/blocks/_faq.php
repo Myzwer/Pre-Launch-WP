00ce8b78aae21b4042ccaba44bd13307600817b8
Myzwer <josh@onefortyfivedesign.com>
2026-05-31 23:19:04 -0400

    :repeat: Refactor FAQ block for improved structure and styling
    Reorganized the FAQ block with a grid-based structure and enhanced utility classes for better readability and maintainability. Removed the unused `$header` variable and updated markup to ensure consistent styling and semantic alignment.

--- flex/blocks/_faq.php
+++ flex/blocks/_faq.php
@@ -22,32 +22,35 @@
 	}
 
 
-	$header = get_sub_field( 'header' );
-	$intro  = get_sub_field( 'intro' );
+	$intro = get_sub_field( 'intro' );
 ?>
-<section class="flex-block flex-block--faq">
-	<?php if ( $header ) : ?>
-		<h2><?php echo esc_html( $header ); ?></h2>
-	<?php endif; ?>
 
-	<?php if ( $intro ) : ?>
-		<div><?php echo wp_kses_post( $intro ); ?></div>
-	<?php endif; ?>
-
-	<?php if ( have_rows( 'faqs' ) ) : ?>
-		<div>
-			<?php while ( have_rows( 'faqs' ) ) : the_row(); ?>
-				<article>
-					<?php $question = get_sub_field( 'faq_question' ); ?>
-					<?php $answer = get_sub_field( 'faq_answer' ); ?>
-					<?php if ( $question ) : ?>
-						<h3><?php echo esc_html( $question ); ?></h3>
-					<?php endif; ?>
-					<?php if ( $answer ) : ?>
-						<p><?php echo nl2br( esc_html( $answer ) ); ?></p>
-					<?php endif; ?>
-				</article>
-			<?php endwhile; ?>
+<section class="wrap">
+	<div class="py-8 grid-12 prose-theme">
+		<div class="col-span-12">
+			<?php if ( $intro ) : ?>
+				<div><?php echo wp_kses_post( $intro ); ?></div>
+			<?php endif; ?>
 		</div>
-	<?php endif; ?>
+
+		<div class="col-span-12">
+			<div class="grid-12">
+				<?php if ( have_rows( 'faqs' ) ) : ?>
+
+				<?php while ( have_rows( 'faqs' ) ) : the_row(); ?>
+					<div class="col-span-12">
+						<?php $question = get_sub_field( 'faq_question' ); ?>
+						<?php $answer = get_sub_field( 'faq_answer' ); ?>
+						<?php if ( $question ) : ?>
+							<h3><?php echo esc_html( $question ); ?></h3>
+						<?php endif; ?>
+						<?php if ( $answer ) : ?>
+							<p><?php echo nl2br( esc_html( $answer ) ); ?></p>
+						<?php endif; ?>
+					</div>
+				<?php endwhile; ?>
+			</div>
+			<?php endif; ?>
+		</div>
+	</div>
 </section>
