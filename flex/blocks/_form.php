460605b5c9e767b77da94c29fc9a80fe1b5b7767
Myzwer <josh@onefortyfivedesign.com>
2026-05-31 23:36:22 -0400

    :repeat: Refactor form block for improved structure and styling
    Updated the form block to use a grid-based layout with enhanced utility classes for better readability and maintainability. Removed the unused `$header` variable, reorganized PHP logic, and improved the semantic alignment of the markup.

--- flex/blocks/_form.php
+++ flex/blocks/_form.php
@@ -22,22 +22,27 @@
 	}
 
 
-	$header  = get_sub_field( 'header' );
 	$intro   = get_sub_field( 'intro' );
 	$form_id = get_sub_field( 'form_id' );
 ?>
-<section class="flex-block flex-block--form">
-	<?php if ( $header ) : ?>
-		<h2><?php echo esc_html( $header ); ?></h2>
-	<?php endif; ?>
 
-	<?php if ( $intro ) : ?>
-		<div><?php echo wp_kses_post( $intro ); ?></div>
-	<?php endif; ?>
-
-	<?php if ( $form_id ) : ?>
-		<div>
-			<?php echo do_shortcode( '[gravityform id="' . absint( $form_id ) . '" title="false" description="false" ajax="true"]' ); ?>
+<section class="wrap">
+	<div class="py-8 grid-12 prose-theme">
+		<div class="col-span-12">
+			<?php if ( $intro ) : ?>
+				<div><?php echo wp_kses_post( $intro ); ?></div>
+			<?php endif; ?>
 		</div>
-	<?php endif; ?>
+
+		<div class="col-span-12">
+			<?php if ( $form_id ) : ?>
+				<div>
+					<?php echo do_shortcode( '[gravityform id="' . absint( $form_id ) . '" title="false" description="false" ajax="true"]' ); ?>
+				</div>
+			<?php endif; ?>
+		</div>
+	</div>
 </section>
+
+
+
